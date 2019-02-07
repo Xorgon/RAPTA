cda7c59f462ccd55ae154d58b123d4cec23c82ae
Xorgon <norman.reggox@gmail.com>
2019-02-07 22:10:14 +0000

    Added PWM reading test.

---
+++ Testing/TestReadPWM/TestReadPWM.ino
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include "Servo.h"
+
+Servo pwmOut;
+uint32_t lastPWMTime;
+uint16_t pwmValue;
+
+void setup() {
+    Serial.begin(57600);
+    attachInterrupt(digitalPinToInterrupt(2), onFalling, FALLING);
+    pwmOut.attach(4);
+    pwmOut.writeMicroseconds(1900);
+}
+
+void loop() {
+    Serial.println(pwmValue);
+}
+
+void onRising() {
+    attachInterrupt(digitalPinToInterrupt(2), onFalling, FALLING);
+    lastPWMTime = micros();
+}
+
+void onFalling() {
+    attachInterrupt(digitalPinToInterrupt(2), onRising, RISING);
+    pwmValue = micros() - lastPWMTime;
+}
